eda6d15c65daefe199e606237b1a5119a051af8a
honeypot <eric@thezombiecoder.com>
2023-03-23 15:09:38 -0700

    refactored inserting categories into its own module

--- script.js
+++ script.js
@@ -1,36 +1,7 @@
 import getJokeInfo from "./modules/jokeInfo.js"
-const figuresBox = document.querySelector("#figures-box") 
+import insertCategories from "./modules/insertCategories.js"
 
 getJokeInfo()
-  .then(function(jokeInfo) {
-    console.log(jokeInfo)
-    // 1. We need to display the supported categories 
-    jokeInfo.categories.forEach(function(element){
-      if(element === "Any") {
-        return null
-      }
-      console.log(element)
-      // create a figure element
-      const figure = document.createElement("figure") 
-      // create 3 children elements, which will be contained in figure
-      const img = document.createElement("img");
-      const h3 = document.createElement("h3");
-      const div = document.createElement("div");
+  .then(insertCategories)
 
-      // Add atributes and/or content to the children 
-      img.src = `./images/categories/${element}.png` 
-      h3.textContent = `${element}`  
-
-      // Insert the 3 children elements into figure
-      figure.insertAdjacentElement("beforeend", img)
-      figure.insertAdjacentElement("beforeend", h3)
-      figure.insertAdjacentElement("beforeend", div)
-
-      // Insert the figure into the #figures-box
-      figuresBox.insertAdjacentElement("beforeend", figure)
-      
-    })
-  })
-
-// jokeInfo.then(results => console.log(results))
 
